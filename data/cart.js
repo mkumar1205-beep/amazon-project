ad95231649d36cbc59477efe46c599f6ac5e270c
Manya Kumar <manyakumar1205@gmail.com>
2026-02-08 23:46:07 +0530

    Save cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,7 @@
- export let cart=[{
+ export let cart=JSON.parse(localStorage.getItem('cart'));
+ 
+ if(!cart){
+  cart=[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity:2
  },
@@ -6,6 +9,12 @@
   productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity:1
 }];
+}
+ 
+
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart));
+}
 
  export function addToCart(productId)
  {
@@ -29,6 +38,7 @@
        quantity:1
        });
      }
+     saveToStorage();
  }
  
  export function removeFromCart(productId){
@@ -41,4 +51,6 @@
   });
 
   cart = newCart;
+
+  saveToStorage();
  }
