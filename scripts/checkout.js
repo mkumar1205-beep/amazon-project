3df06d22e79c243906441767211ee4833270aa1e
Manya Kumar <manyakumar1205@gmail.com>
2026-02-08 23:37:35 +0530

    Make delete button interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -14,7 +14,8 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML +=`
-   <div class="cart-item-container">
+   <div class="cart-item-container 
+   js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -37,7 +38,8 @@ cart.forEach((cartItem) => {
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary js-delete-link data-product-id=${matchingProduct.id}">
+                  <span class="delete-quantity-link link-primary js-delete-link"
+                  data-product-id="${matchingProduct.id}">
                     Delete
                   </span>
                 </div>
@@ -98,5 +100,10 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click' , () =>{
     const productId=link.dataset.productId;
     removeFromCart(productId);
-  })
+
+    const container=document.querySelector(
+    `.js-cart-item-container-${productId}`);
+
+    container.remove();
+  });
 });
