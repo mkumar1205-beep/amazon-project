0682dce1e911aacd522890db208b4ad86c98222a
Manya Kumar <manyakumar1205@gmail.com>
2026-02-10 20:54:11 +0530

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,10 +3,7 @@ import {products, getProduct} from'../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
-const today=dayjs();
-const deliveryDate=today.add(7,'days');
-deliveryDate.format('dddd, MMMM D');
+import {renderPaymentSummary} from './paymentSummary.js';
 
 export function renderOrderSummary() {
   let cartSummaryHTML='';
@@ -116,6 +113,8 @@ export function renderOrderSummary() {
       `.js-cart-item-container-${productId}`);
 
       container.remove();
+
+      renderPaymentSummary();
     });
   });
 
@@ -124,6 +123,7 @@ export function renderOrderSummary() {
       const {productId, deliveryOptionId} =element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
